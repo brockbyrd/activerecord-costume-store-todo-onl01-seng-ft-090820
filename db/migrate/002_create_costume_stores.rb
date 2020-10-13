8b4a2a005eda4023bbac2a120705725c16786140
Brock Byrd <brockbyrdd@gmail.com>
2020-10-13 14:06:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,15 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores
-
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :employees
+      t.boolean :open
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
 end
