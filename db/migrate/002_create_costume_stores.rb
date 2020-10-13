9d12db57b567a5ab276f4c5eb07c388a1a4270b0
Brock Byrd <brockbyrdd@gmail.com>
2020-10-13 14:10:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :employees
-      t.boolean :open
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
