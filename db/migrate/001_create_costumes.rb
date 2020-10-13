7ccf2cdeb9727fa6a1cfcad66019665d292eaa42
Brock Byrd <brockbyrdd@gmail.com>
2020-10-13 14:14:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :image_url
       t.string :size
       t.timestamps
