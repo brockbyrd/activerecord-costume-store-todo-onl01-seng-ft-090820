8dfe5341b8d62f5d1576419de7a2cdca7140f688
Brock Byrd <brockbyrdd@gmail.com>
2020-10-13 14:20:31 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,17 @@
 # Create your haunted_houses migration here
 
 class CreateHauntedHouse
+  def change
+    create_table :haunted_house do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+  end
 
 end
